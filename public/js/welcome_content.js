90c9ee0ec974912dc8db62ebeffbc03272612622
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-09 17:47:05 -0500

    add files

---
+++ public/js/welcome_content.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.welcome_content form').submit(function(event){
+    event.preventDefault();
+    var title = $(this).find('input');
+    var title_value = {title: title.val()};
+    var request = $.ajax({
+      url: $(this).attr('action'),
+      method: 'POST',
+      data: title_value
+    });
+
+    request.done(function(response){
+      // $('.welcome_victory').replaceWith(response);
+      $('.welcome_victory').remove();
+      $(response).hide().appendTo('.welcome_content').slideToggle("slow");
+      title.val('');
+    });
+  });
+});
