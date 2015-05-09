4db88f5ec73db14d103a772551e2842b6f4684b7
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-09 12:55:01 -0500

    updated index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,12 @@
 get '/' do
   if current_user == nil
     length = Victory.all.length
-    @victory = Victory.find_by_id(rand(length))
+    @victory = Victory.find_by_id(rand(length-2))
     @victory_random_three = Victory.all.sample(3)
   else
     user = User.find_by_id(current_user.id)
     victories = user.victories
-    @victory = victories[rand(victories.length)]
+    @victory = victories[rand(victories.length-2)]
     @victory_random_three = [Victory.last, Victory.first, Victory.last]
   end
   @victory_random_four = Victory.all.sample(4)
--- db/migrate/20150508201816_create_taggings.rb
+++ db/migrate/20150508201816_create_taggings.rb
@@ -1,7 +1,6 @@
 class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
-      t.string     :name, null: false
       t.belongs_to :victory
       t.belongs_to :tag
       t.timestamps null: false
