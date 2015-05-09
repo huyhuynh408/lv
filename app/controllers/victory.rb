629208a31458510865439956bc52501a397774ca
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-09 10:07:05 -0500

    added victory form

--- app/controllers/victory.rb
+++ app/controllers/victory.rb
@@ -1,15 +1,15 @@
-get '/users/:id/victories/new' do
-  "Hello World"
+post '/users/victories/new' do
+  if current_user.id == nil
+    redirect('/')
+  else
+    Victory.create(title: params[:title], user_id: current_user.id)
+  end
 end
 
-post '/users/:id/victories/new' do
+get '/users/victories/all' do
   "Hello World"
 end
 
 get '/users/:id/victories' do
-  "Hello World"
-end
-
-get '/users/:id/victories/all' do
   "Hello World"
 end
