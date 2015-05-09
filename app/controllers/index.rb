d380d3ec6b5e3a5f64e0d7ca253fe7f1574f78f3
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-08 21:55:46 -0500

    more css

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,6 @@
 get '/' do
-  @victory = Victory.first
+  length = Victory.all.size
+  @victory = Victory.find_by_id(rand(length))
+  @victory_random_three = [Victory.last, Victory.last, Victory.last]
   erb :index
 end
