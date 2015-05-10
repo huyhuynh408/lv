ce084a0ad160626ac7d3809743590b0784c1bc77
Huy Huynh <huyhuynh85@gmail.com>
2015-05-10 16:16:11 -0500

    added js for select

--- public/js/welcome_content.js
+++ public/js/welcome_content.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
     });
 
     request.done(function(response){
-      // $('.welcome_victory').replaceWith(response);
+      $('select').hide();
+      $('.add_tag_button').show();
       $('.welcome_victory').remove();
       $(response).hide().appendTo('.welcome_content').slideToggle("slow");
       title.val('');
@@ -19,21 +20,9 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $('.welcome_content form').submit(function(event){
-    event.preventDefault();
-    var title = $(this).find('input');
-    var title_value = {title: title.val()};
-    var request = $.ajax({
-      url: $(this).attr('action'),
-      method: 'POST',
-      data: title_value
-    });
-
-    request.done(function(response){
-      // $('.welcome_victory').replaceWith(response);
-      $('.welcome_victory').remove();
-      $(response).hide().appendTo('.welcome_content').slideToggle("slow");
-      title.val('');
-    });
+  $('select').hide();
+  $('.add_tag_button').click(function(){
+    $('select').show();
+    $('.add_tag_button').hide();
   });
 });
