d16beed60cbde868c0a039b45e169e9bbeb1553f
Huy Huynh <huyhuynh85@gmail.com>
2015-05-11 17:36:49 -0500

    fixed tag js

--- public/js/welcome_content.js
+++ public/js/welcome_content.js
@@ -24,6 +24,6 @@ $(document).ready(function(){
   $('.add_tag_button').click(function(){
     $('select').slideDown();
     $('.add_tag_button').hide();
-    $("option[value='please_select']").replaceWith("<option value='please_select' selected='selected'>Please select a tag</option>")
+    $("option[value='please_select']").replaceWith("<option selected='selected'>Please select a tag</option>")
   });
 });
